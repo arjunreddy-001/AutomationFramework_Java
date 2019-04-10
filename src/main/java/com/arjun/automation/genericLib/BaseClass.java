f8e0edee61e5ddc261a03776f9ca47b36580a77a
Arjun Reddy <arjun.reddy@ad.starmarkit.com>
2019-04-10 19:09:20 +0530

    Added guice to pom.xml Optimized BaseClass Title parameter added to text.xml

--- src/main/java/com/arjun/automation/genericLib/BaseClass.java
+++ src/main/java/com/arjun/automation/genericLib/BaseClass.java
@@ -1,6 +1,7 @@
 package com.arjun.automation.genericLib;
 
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
@@ -10,16 +11,33 @@ public class BaseClass
 	public WebDriver driver;
 
 	@BeforeClass
-	@Parameters({ "browser", "OS", "url" })
-	public void setup(String browser, String OS, String url)
+	@Parameters({ "browser", "OS", "url", "title" })
+	public void setup(String browser, String OS, String url, String title)
 	{
 		driver = new Driver(driver).getDriver(browser, OS);
-		driver.get(url);
-		driver.navigate().refresh();
+		
+		int maxAttempts = 3;
+		int AttemptNum = 0;
+		do
+		{
+			driver.get(url);
+			AttemptNum++;
+			
+			if(driver.getTitle().contains(title))
+			{
+				break;
+			}
+			
+			if(AttemptNum == maxAttempts)
+			{
+				Assert.assertEquals(title, driver.getTitle(), "Failed to load URL: " + url + ", WebPage title not matched");
+			}
+		}
+		while((!driver.getTitle().contains(title)));
 	}
 
 	@AfterClass
-	public void tearDown() 
+	public void tearDown()
 	{
 		driver.quit();
 	}
